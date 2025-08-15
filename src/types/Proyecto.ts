fd8e99b163949c56a034bbefde1c37b273374133
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-08-15 16:03:33 -0400

    Commit inicial portafolio 2025 - React + TS

---
+++ src/types/Proyecto.ts
@@ -0,0 +1,8 @@
+export interface Proyecto {
+  id: string
+  titulo: string
+  descripcion: string
+  tecnologia: 'ReactJS' | 'React Native' | 'NestJS' | 'IA Generativa'
+  imagen: string
+  enlace: string
+}
